3c2675dac24488134c248629732bf4d5a567d4a1
Akin <uche@imaginebusiness.com.ng>
2022-01-28 16:44:47 +0100

    Starting Salactiv erp in elixir

--- lib/salactiv_erp_web/router.ex
+++ lib/salactiv_erp_web/router.ex
@@ -18,6 +18,7 @@ defmodule SalactivErpWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/login", PageController, :login
   end
 
   # Other scopes may use custom stacks.
